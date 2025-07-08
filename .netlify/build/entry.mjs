01a958f442f81aad62f0a8292faf59a009e4a22a
Paige <paigewilley@protonmail.com>
2025-07-07 20:00:18 -0600

    upgrade astro version
    ignore netlify

---
+++ .netlify/build/entry.mjs
@@ -0,0 +1,34 @@
+import { renderers } from './renderers.mjs';
+import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
+import { manifest } from './manifest_Bi9Ui7wc.mjs';
+import { createExports } from '@astrojs/netlify/ssr-function.js';
+
+const serverIslandMap = new Map();;
+
+const _page0 = () => import('./pages/posts/hello-world.astro.mjs');
+const _page1 = () => import('./pages/posts.astro.mjs');
+const _page2 = () => import('./pages/index.astro.mjs');
+const pageMap = new Map([
+    ["src/pages/posts/hello-world.md", _page0],
+    ["src/pages/posts/index.astro", _page1],
+    ["src/pages/index.astro", _page2]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    actions: () => import('./_noop-actions.mjs'),
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "dfcb3687-009b-4072-ae03-c1ddda7cee16"
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+const _start = 'start';
+if (_start in serverEntrypointModule) {
+	serverEntrypointModule[_start](_manifest, _args);
+}
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
